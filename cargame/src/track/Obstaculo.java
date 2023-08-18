6d12a27ff7fbe1e59f172568f309aa6b4e37776e
Arturo Chavarria <chavarriavillareviajorgearturo@gmail.com>
2023-08-18 17:29:38 -0600

    subiendo archivos

--- cargame/src/track/Obstaculo.java
+++ cargame/src/track/Obstaculo.java
@@ -2,4 +2,14 @@ package track;
 
 public class Obstaculo {
 
+	private String Tipo;
+	private int length;
+	private int damage;
+	
+	public Obstaculo(String pTipo) {
+		this.Tipo = pTipo;
+		length = 3;
+		damage = 10;
+	}
+
 }
